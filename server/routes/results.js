78812d7b2f13594ad847ba45fed44526882d271c
faan234 <74786729+faan234@users.noreply.github.com>
2022-01-29 19:03:03 -0600

    Update results.js
    edit1

--- server/routes/results.js
+++ server/routes/results.js
@@ -1,9 +1,9 @@
 const express = require('express');
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 const config = require('../config/config.json');
 const router = express.Router();
 
-// Future TODO: set up mysql database
+// TODO: set up mysql database
 // const connection = mysql.createConnection({
 //   host: config.development.host,
 //   user: config.development.username,
@@ -19,6 +19,40 @@ const router = express.Router();
 async function result(req, res) {
   // TODO: Angela implement and return json of results
   // I will give the data as a string req="-80,-80"
+ //read json
+ let response = await fetch('/server/test_data/star_data_yes.json');
+ let user = await response.json(); 
+ //call helper function
+  ret = parseReq(req); 
+//res.json()
+  res = user; 
+  //return results
+  return res;  
+}
+
+//parse the req and return true 
+function parseReq(req) {
+  
+  //sep by comma
+  const coordinate = req.split(',');
+  //test
+  let latitude = word[0]; 
+  //console.log(word[0]); 
+  let longitude = word[1]; 
+  //console.log(word[1]);
+
+  //if found in star_data_yes, return true
+  //var latitude = await User.
+  if (latitude && longitude) {
+      let y = new Boolean(true);
+      return y;
+  } 
+  if (!latitude || !longitude) {
+      let x = new Boolean(false);
+      return x; 
+  }
+  //if found in star_data_no
+
 }
 
 module.exports = {
